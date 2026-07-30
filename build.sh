ced60a65c98da7ed1d29d91932f112060df56e18
n07name7 <ivan.fediukov@gmail.com>
2026-07-30 20:23:54 +0200

    Auto-load accidents during deployment

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py createcachetable 2>/dev/null || true
+python manage.py load_accidents --months 24
